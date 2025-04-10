f92b88e034a79908ba7f7d9e15231acbcc0058b4
kalobmj <kalobmurphyjones@gmail.com>
2025-04-10 17:18:14 -0400

    main

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   // base line screws up our images
   // base: '/osrs-price-catcher/',
+  base: process.env.NODE_ENV === 'production' ? '/osrs-price-catcher/' : '/',
   plugins: [react()],
   server: {
     proxy: {
